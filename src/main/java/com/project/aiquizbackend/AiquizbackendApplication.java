1d00487f63c86fb52f68ae9b11513aebfad639a9
Pratiksha Tidke <tidkepratiksha11@gmail.com>
2025-09-05 23:04:11 +0530

    refactor: Update properties for deployment

--- src/main/java/com/project/aiquizbackend/AiquizbackendApplication.java
+++ src/main/java/com/project/aiquizbackend/AiquizbackendApplication.java
@@ -2,8 +2,10 @@ package com.project.aiquizbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class AiquizbackendApplication {
 
 	public static void main(String[] args) {
